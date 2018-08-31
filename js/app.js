4355b36ee33fbb44f08f016912766e946f07d1c7
bcicchese <b.cicchese@gmail.com>
2018-08-31 16:03:42 -0600

    Key inputs added

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 // Enemy class. Player must avoid enemies
 class Enemy {
   constructor() {
-    // Variables applied to each of our instances go here,
+    // Variables applied to each of our instances go here
     this.x = 0;
     this.y = 0;
     this.sprite = 'images/enemy-bug.png';
@@ -9,9 +9,8 @@ class Enemy {
   // Update the enemy's position, required method for game
   // Parameter: dt, a time delta between ticks
   update(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+    // Multiply any movement by the dt parameter, ensuring the game runs at the same speed for all computers.
+    this.x += (this.speed * dt);
   }
   // Draw the enemy on the screen, required method for game
   render() {
@@ -22,31 +21,52 @@ class Enemy {
 // Player class with update(), render() and handleInput() methods.
 class Player {
   constructor() {
-    this.x = 0;
-    this.y = 0;
+    this.x = 202;
+    this.y = 405;
     this.sprite = 'images/char-cat-girl.png';
   }
   update() {
+    //check for collision
+    //check if water has been reached
 
   }
+  //Draw the player on the screen
   render() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
   }
-  handleInput(){
-
+  //handle key input from event listener below
+  handleInput(keys){
+    switch(keys){
+        case 'up':
+            if (this.y > 0) {
+                this.y -= 83;
+            };
+            break;
+        case 'down':
+            if (this.y < 405) {
+                this.y += 83;
+            };
+            break;
+        case 'left':
+            if (this.x >0) {
+                this.x -= 101;
+            };
+            break;
+        case 'right':
+            if (this.x < 404) {
+                this.x += 101;
+            };
+            break;
+    }
   }
 }
 
-
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+// Instantiate objects.
 const allEnemies = [new Enemy()];
 const player = new Player();
 
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+// Listens for key presses and sends the keys to Player.handleInput() method.
 document.addEventListener('keyup', function(e) {
   const allowedKeys = {
     37: 'left',
