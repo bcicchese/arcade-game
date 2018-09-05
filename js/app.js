c1808087688cda9773866031d4532e3070170244
bcicchese <b.cicchese@gmail.com>
2018-09-05 16:33:27 -0600

    Collision logic in place

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 class Enemy {
   constructor() {
     // Variables applied to each of our instances go here
-    this.x = 100;
+    this.x = -101;
     this.y = 83 * Math.floor(Math.random() * 3) + 65;
     this.speed = 60 * Math.floor(Math.random() * 3 + 1);
     this.sprite = 'images/enemy-bug.png';
@@ -13,16 +13,16 @@ class Enemy {
     // Multiply any movement by the dt parameter, ensuring the game runs at the same speed for all computers.
     this.x += (this.speed * dt);
     if (this.x > 505) {
-      this.x = -100;
+      this.x = -101;
       this.y = 83 * Math.floor(Math.random() * 3) + 65;
       this.speed = 60 * Math.floor(Math.random() * 3 + 1);
     }
     // Check for collision with Player
     if (
-      player.x >= this.x &&
-      player.x <= this.x &&
-      player.y >= this.y &&
-      player.y <= this.y
+      player.x >= this.x - 83 &&
+      player.x <= this.x + 83 &&
+      player.y >= this.y - 20 &&
+      player.y <= this.y + 80
     ) {
       //Return player to starting position
       player.x = 202;
@@ -43,18 +43,22 @@ class Player {
     this.sprite = 'images/char-cat-girl.png';
   }
   update(){
+    //check to see if the player has reached the water
     if (this.y === -10) {
       this.x = 202;
       this.y = 405;
-      allEnemies.push(new Enemy());
-    }  
+      //add a new enemy to board (max of 8)
+      if(allEnemies.length < 8){
+        allEnemies.push(new Enemy());
+      }
+    }
   }
 
   //Draw the player on the screen
   render() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
   }
-  //handle key input from event listener below
+  //handle key input from event listener below. The if statements provide boundaries so the Player doesn't move off the board
   handleInput(keys){
     switch(keys){
       case 'up':
