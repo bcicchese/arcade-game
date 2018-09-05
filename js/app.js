b3c53978079d62b42af0f80deb60690ac04760fc
bcicchese <b.cicchese@gmail.com>
2018-09-05 15:59:38 -0600

    Working on collision logic...

--- js/app.js
+++ js/app.js
@@ -13,12 +13,20 @@ class Enemy {
     // Multiply any movement by the dt parameter, ensuring the game runs at the same speed for all computers.
     this.x += (this.speed * dt);
     if (this.x > 505) {
-        this.x = -100;
-        this.y = 83 * Math.floor(Math.random() * 3) + 65;
-        this.speed = 60 * Math.floor(Math.random() * 3 + 1);
+      this.x = -100;
+      this.y = 83 * Math.floor(Math.random() * 3) + 65;
+      this.speed = 60 * Math.floor(Math.random() * 3 + 1);
     }
     // Check for collision with Player
-
+    if (
+      player.x >= this.x &&
+      player.x <= this.x &&
+      player.y >= this.y &&
+      player.y <= this.y
+    ) {
+      //Return player to starting position
+      player.x = 202;
+      player.y = 405;
     }
   }
   // Draw the enemy on the screen, required method for game
@@ -34,9 +42,14 @@ class Player {
     this.y = 405;
     this.sprite = 'images/char-cat-girl.png';
   }
-  update()
-    //check if water has been reached
+  update(){
+    if (this.y === -10) {
+      this.x = 202;
+      this.y = 405;
+      allEnemies.push(new Enemy());
+    }  
   }
+
   //Draw the player on the screen
   render() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
@@ -44,26 +57,26 @@ class Player {
   //handle key input from event listener below
   handleInput(keys){
     switch(keys){
-        case 'up':
-            if (this.y > 0) {
-                this.y -= 83;
-            };
-            break;
-        case 'down':
-            if (this.y < 405) {
-                this.y += 83;
-            };
-            break;
-        case 'left':
-            if (this.x > 0) {
-                this.x -= 101;
-            };
-            break;
-        case 'right':
-            if (this.x < 404) {
-                this.x += 101;
-            };
-            break;
+      case 'up':
+      if (this.y > 0) {
+        this.y -= 83;
+      };
+      break;
+      case 'down':
+      if (this.y < 405) {
+        this.y += 83;
+      };
+      break;
+      case 'left':
+      if (this.x > 0) {
+        this.x -= 101;
+      };
+      break;
+      case 'right':
+      if (this.x < 404) {
+        this.x += 101;
+      };
+      break;
     }
   }
 }
